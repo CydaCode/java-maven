09854f6892edea4970332b74e9ca352d77677cb4
cydacode <nwacynti25@gmail.com>
2025-12-02 21:34:37 +0100

    Changes on the script file

---
+++ script.groovy
@@ -0,0 +1,23 @@
+def buildApp() {
+    echo 'building application'
+    sh 'mvn package'
+}
+
+def buildImage() {
+    echo 'building application'
+    withCredentials([
+        usernamePassword(credentialsId: 'docker-hub-id', usernameVariable: 'USER', passwordVariable: 'PASSWORD')
+    ]) {
+        sh '''
+            docker build -t cloudqween/private_repo:jma.2.0 .
+            echo $PASSWORD | docker login -u $USER --password-stdin
+            docker push cloudqween/private_repo:jma.2.0
+            '''
+        }
+}
+
+def deployApp() {
+    echo 'deploying application'
+}
+
+return this
